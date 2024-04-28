91a2395a4e2e6b41fc184cdb229e68ecbc1c08d0
Dulce Figueredo <dulcefigueredo2252@gmail.com>
2024-04-28 17:58:09 -0400

    Added a search form and some basic styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
+import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-function App(props) {
-  
-  function handleResponse(response) {
-    alert(`The weather in ${props.city} is ${response.data.main.temp} °C`);
+function App() {
+  let [city, setCity] = useState("");
+  let [weather, setWeather] = useState({});
+  let [data, setData] = useState(false);
+
+  function SearchCity(event) {
+    event.preventDefault();
+
+    let apiKey = "210d99196a88b9257ed8cb3535a0a0c5";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(ShowWeather);
   }
 
-  let apiKey = "210d99196a88b9257ed8cb3535a0a0c5";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
 
-  axios.get(apiUrl).then(handleResponse);
+  function ShowWeather(response) {
+    setData(true);
+    setWeather({
+      name: response.data.name,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hello with React!!!</h2>
-      </header>
+  let form = (
+    <div className="form">
+      <form onSubmit={SearchCity}>
+        <input
+          className="search-city"
+          type="search"
+          placeholder="Search a city..."
+          autoComplete="off"
+          onChange={updateCity}
+        />
+        <input className="search-button" type="submit" value="Search" />
+      </form>
     </div>
   );
+
+  if (data) {
+    return (
+      <div className="weather-container">
+        {form}
+        <div className="weather-details">
+          <p>City: {weather.name}</p>
+          <p>Temperature: {Math.round(weather.temperature)} °C</p>
+          <p>Description: {weather.description}</p>
+          <p>Humidity: {weather.humidity} %</p>
+          <p>Wind: {weather.wind} km/h</p>
+
+          <img src={weather.icon} alt={weather.description} />
+        </div>
+        <hr></hr>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App city="France"/>
+    <div className="weather-app">
+      <App />
+      <p className="foot">
+        Open-source code on{" "}
+        <a href="https://github.com/DulcePy/weather-react">GitHub</a>, by Dulce
+        Figueredo
+      </p>
+    </div>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
